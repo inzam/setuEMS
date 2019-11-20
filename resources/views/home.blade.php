4d7af64338e5d38d01472ba9deffa393f80d71ea
inzam <inzam969zaman@gmail.com>
2019-11-21 02:51:02 +0600

    home page changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <a href="/official" class="btn btn-block">Official Information</a>
-            <a href="/personal" class="btn btn-block">Personal Information</a>
+        <div class="col-md-8 mt-5">
+            <a href="/official" class="btn btn-outline-info btn-block my-2" style="font-size: larger">Official Information</a>
+            <a href="/personal" class="btn btn-outline-info btn-block my-2" style="font-size: larger">Personal Information</a>
+            <a href="#" class="btn btn-outline-info btn-block my-2" style="font-size: larger">HR Information</a>
+            <a href="#" class="btn btn-outline-info btn-block my-2" style="font-size: larger">Job Performance Analysis</a>
+            <a href="#" class="btn btn-outline-info btn-block my-2" style="font-size: larger">Accounts Information</a>
         </div>
     </div>
 </div>
